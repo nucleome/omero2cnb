68e672aa78478e321a5821576c342a6f450c5537
zhuxp <nimezhu@gmail.com>
2019-10-01 12:28:04 -0400

    fix list cors

--- manager.go
+++ manager.go
@@ -14,7 +14,6 @@ type Manager struct {
 
 func (m *Manager) ServeTo(r *mux.Router) {
 	r.HandleFunc(m.prefix+"/list", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
 		keys := []string{}
 		for _, v := range m.Data {
 			keys = append(keys, v.Name)
